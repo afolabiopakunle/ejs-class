79c11242f14b4dd891c6d6aeb048cb927f6fe606
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-06 15:31:45 +0100

    home ejs added

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    let students = [
+        {name: "Afolabi", age: 34},
+        {name: "Caroline", age: 24},
+        {name: "Dele", age: 31},
+    ]
+    res.render('home.ejs', {students});
+})
+
+app.get('/:home/:area/', (req, res) => {
+    console.log(req.params);
+    res.send('Parameters Page');
+})
+
+app.get('/dogs', function (req, res) {
+    res.render('dogs.ejs');
+})
+
+app.listen(3000, console.log('site opening on port 3000'));

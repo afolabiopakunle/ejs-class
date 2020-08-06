31147bb64e04a2ab67c520c342cd55c26206b147
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-06 16:34:05 +0100

    public directory added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"))
+
 app.get('/', function (req, res) {
     let students = [
         {name: "Afolabi", age: 34},
